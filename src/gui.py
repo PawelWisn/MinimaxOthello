a48420290cf253c3f7abf7998b86ab45ce16b4d8
Pawel <wispawelwis38@gmail.com>
2020-05-01 21:42:17 +0200

    basic menu

--- src/gui.py
+++ src/gui.py
@@ -10,7 +10,7 @@ class abcSquare(ABC):
         self.y = y
         self.size = size
         self.kind = squareImg
-        self.photo=None
+        self.photo = None
         self.occupied = False
 
     def __repr__(self):
@@ -43,5 +43,27 @@ class Window(tk.Tk):
         self.title(title)
         self.geometry(f'{width}x{height}')
 
+        self.startButton = tk.Button(self, text='Start')
+        self.startButton.grid(row=1, column=9)
+        self.restartButton = tk.Button(self, text='Restart')
+        self.restartButton.grid(row=2, column=9)
+        isAIon = False
+        self.AIon = tk.Checkbutton(self, text='  AI', variable=isAIon)
+        self.AIon.grid(row=3, column=9, sticky=tk.W)
+
+        self.depthLabel = tk.Label(self, text="Depth:")
+        self.depthLabel.grid(row=5, column=9, sticky=tk.W)
+        self.depthInput = tk.Entry(self, width=6)
+        self.depthInput.grid(row=5, column=10, sticky=tk.W)
+        self.depthSubmit = tk.Button(self, text='Set')
+        self.depthSubmit.grid(row=5, column=11, sticky=tk.W)
+
+        heurVar = tk.IntVar()
+        self.heurLabel = tk.Label(self, text="Heuristic:")
+        self.heurLabel.grid(row=7, column=9)
+        self.heuristicRadio1 = tk.Radiobutton(self, text="Naive", variable=heurVar, value=1)
+        self.heuristicRadio2 = tk.Radiobutton(self, text="Advanced", variable=heurVar, value=2)
+        self.heuristicRadio1.grid(row=7,column=10)
+        self.heuristicRadio2.grid(row=7,column=11)
     def run(self):
         self.mainloop()
--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from src.gui import Window
 from src.reversi import Square, Board
 
-window = Window(1150, 800, 'Reversi')
+window = Window(1000, 750, 'Reversi')
 board = Board(window, Square, 64, 75)
 window.run()
