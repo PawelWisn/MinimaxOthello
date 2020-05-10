2db6d9bea1e1842d31d9ab5002f7f5ef2876397f
Pawel <wispawelwis38@gmail.com>
2020-05-10 03:51:28 +0200

    minimax return the best move

--- src/minimax.py
+++ src/minimax.py
@@ -5,15 +5,14 @@ from copy import deepcopy
 class Minimax:
     def __init__(self, game, alphaBeta=False):
         self.game = deepcopy(game)
-        self.gameCopy = game
         self.alphaBeta = alphaBeta
         self.heuristic = game.getHeuristic()
         self.depth = game.settings.getDepthP1() if game.currPlayer is game.player1 else game.settings.getDepthP2()
         self.inf = int(1e50)
+        self.moveDict = {}
 
     def getBestMove(self):
         out = self.search(self.game, self.depth, self.game.currPlayer is self.game.player1)
-        self.game = self.gameCopy
         return out
 
     def search(self, game: abcGame, depth: int, maximizing: bool = True):
@@ -27,6 +26,7 @@ class Minimax:
                     copy = deepcopy(game)
                     copy.commitMove(move, display=False)
                     value = max(value, self.search(copy, depth - 1, False))
+                    if depth == self.depth: self.moveDict[move] = value
             else:
                 value = self.search(game, depth - 1, False)
         else:
@@ -36,6 +36,13 @@ class Minimax:
                     copy = deepcopy(game)
                     copy.commitMove(move, display=False)
                     value = min(value, self.search(copy, depth - 1, True))
+                    if depth == self.depth: self.moveDict[move] = value
             else:
                 value = self.search(game, depth - 1, True)
-        return value
+
+        if depth==self.depth:
+            print(self.moveDict.items())
+            if maximizing: return max(self.moveDict.items(),key=lambda x:x[1])
+            else: return min(self.moveDict.items(),key=lambda x:x[1])
+        else:
+            return value
